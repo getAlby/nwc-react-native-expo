9088b360325cd2125afc9fc897d1ae6516e3cac0
Roland Bewick <roland.bewick@gmail.com>
2023-11-13 22:11:07 +0700

    feat: add sendPayment example

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
+import { LightningAddress } from "@getalby/lightning-tools";
 import "./applyGlobalPolyfills";
 
 import { webln } from "@getalby/sdk";
 import React from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from "react-native";
 import PolyfillCrypto from "react-native-webview-crypto";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <PolyfillCrypto />
-      <Balance />
+      <NWCPlayground />
     </SafeAreaView>
   );
 }
@@ -22,23 +29,84 @@ const styles = StyleSheet.create({
   },
 });
 
-function Balance() {
-  const nwcUrl =
-    "nostr+walletconnect://69effe7b49a6dd5cf525bd0905917a5005ffe480b58eeb8e861418cf3ae760d9?relay=wss://relay.getalby.com/v1&secret=99e8fe6bf8777aa7e4ba572ab1d2fab9912bcd781900323cb734a9063927efc3";
+function NWCPlayground() {
+  const [nwcUrl, setNwcUrl] = React.useState("");
+  const [paymentRequest, setPaymentRequest] = React.useState("");
+  const [preimage, setPreimage] = React.useState("");
+  const [nostrWebLN, setNostrWebLN] = React.useState<
+    webln.NostrWebLNProvider | undefined
+  >(undefined);
 
   const [balance, setBalance] = React.useState<number | undefined>();
-  (async () => {
+  React.useEffect(() => {
+    if (!nwcUrl) {
+      return;
+    }
+    (async () => {
+      try {
+        const _nostrWebLN = new webln.NostrWebLNProvider({
+          nostrWalletConnectUrl: nwcUrl,
+        });
+        setNostrWebLN(_nostrWebLN);
+        await _nostrWebLN.enable();
+        const response = await _nostrWebLN.getBalance();
+        console.log("Balance response", response);
+        setBalance(response.balance);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+
+    (async () => {
+      try {
+        const lightningAddress = new LightningAddress("hello@getalby.com");
+        await lightningAddress.fetch();
+        const invoice = await lightningAddress.requestInvoice({
+          satoshi: 1,
+        });
+        setPaymentRequest(invoice.paymentRequest);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [nwcUrl]);
+
+  async function payInvoice() {
     try {
-      const nostrWebln = new webln.NostrWebLNProvider({
-        nostrWalletConnectUrl: nwcUrl,
-      });
-      await nostrWebln.enable();
-      const response = await nostrWebln.getBalance();
-      console.log("Balance response", response);
-      setBalance(response.balance);
+      if (!nostrWebLN) {
+        throw new Error("No WebLN provider");
+      }
+      const result = await nostrWebLN.sendPayment(paymentRequest);
+      setPreimage(result.preimage);
     } catch (error) {
       console.error(error);
     }
-  })();
-  return <Text>Balance: {balance ?? "Loading..."}</Text>;
+  }
+
+  return (
+    <>
+      <Text>NWC URL</Text>
+      <TextInput
+        onChangeText={(text) => setNwcUrl(text)}
+        style={{
+          borderColor: "black",
+          borderWidth: 1,
+          padding: 10,
+          margin: 10,
+        }}
+      />
+      {nwcUrl && (
+        <>
+          <Text>Balance</Text>
+          <Text>{balance ?? "Loading..."}</Text>
+          <Text>Pay an invoice</Text>
+          <Text>{paymentRequest ?? "Loading..."}</Text>
+          {paymentRequest && (
+            <Button title="Pay invoice (1 sat)" onPress={payInvoice} />
+          )}
+          <Text>{preimage ? `PAID: ${preimage}` : "Not paid yet"}</Text>
+        </>
+      )}
+    </>
+  );
 }
