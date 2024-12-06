e8b068abff9965fd3ae14a153711a959c6511fb5
Roland Bewick <roland.bewick@gmail.com>
2024-12-06 15:45:18 +0700

    chore: update dependencies

--- applyGlobalPolyfills.ts
+++ applyGlobalPolyfills.ts
@@ -1,6 +1,7 @@
 const TextEncodingPolyfill = require("text-encoding");
 import "react-native-url-polyfill/auto";
 import "react-native-get-random-values";
+import "message-port-polyfill";
 
 const applyGlobalPolyfills = () => {
   Object.assign(global, {
